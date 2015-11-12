534780ab1440f0c8f7c243400b9eb64aec6e4dad
rafael.cassiano <rafael.cassiano22@gmail.com>
2015-11-12 20:43:45 -0200

    ...

--- src/main/java/br/gov/sp/fatec/WebcoldApplication.java
+++ src/main/java/br/gov/sp/fatec/WebcoldApplication.java
@@ -2,11 +2,13 @@ package br.gov.sp.fatec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.ViewResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 
 @SpringBootApplication
 public class WebcoldApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(WebcoldApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(WebcoldApplication.class, args);
+	}
 }
